085e05c9d55130322fe28b8f0fdc80714db83878
ibrown <ibrown@twitter.com>
2018-11-04 11:34:03 -0800

    refactoring Java template and generator to use Objective-C string formats

--- Fof/JavaCodeGenerator.m
+++ Fof/JavaCodeGenerator.m
@@ -11,22 +11,11 @@
 @implementation JavaCodeGenerator
 
 -(NSString*)generate:(NSArray*)points :(NSString*)name{
-    NSMutableString* code = [[NSMutableString alloc] init];
-    
-    [code appendString:@"import java.awt.geom.*;\n\n"];
-    [code appendString:@"public class "];
-    [code appendString:name];
-    [code appendString:@" {\n"];
-    [code appendString:@"\tpublic static Point2D.Double[] points = {\n"];
     
     NSString* format = @"\t\tnew Point2D.Double(%f, %f)";
-    [code appendString:[self pointsArrayToString:format :points]];
-    [code appendString:@"\t};\n\n"];
-    [code appendString:[self getTemplate:@"JavaTemplate"]];
-    return code;
-    
-    
-    return code;
+
+    return [NSString stringWithFormat:[self getTemplate:@"JavaTemplate"], name, [self pointsArrayToString:format :points]];
+  
 
 }
 
