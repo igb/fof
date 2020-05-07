a015256dad4bf8c0aee309b350cbd7af05057945
ibrown <ibrown@twitter.com>
2020-05-07 10:00:23 -0700

    Initial work to refactor core functionality into libs.

---
+++ libFoF4MacOS/libFoF4MacOS.h
@@ -0,0 +1,13 @@
+//
+//  libFoF4MacOS.h
+//  libFoF4MacOS
+//
+//  Created by Ian Brown  on 5/6/20.
+//  Copyright © 2020 Ian Brown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libFoF4MacOS : NSObject
+
+@end
---
+++ libFoF4MacOS/libFoF4MacOS.m
@@ -0,0 +1,13 @@
+//
+//  libFoF4MacOS.m
+//  libFoF4MacOS
+//
+//  Created by Ian Brown  on 5/6/20.
+//  Copyright © 2020 Ian Brown. All rights reserved.
+//
+
+#import "libFoF4MacOS.h"
+
+@implementation libFoF4MacOS
+
+@end
